cc103689706c2f91b230b70fa701f8d0fd8a0185
Matt Voget <matt.voget@gmail.com>
2015-06-02 21:22:15 -0400

    Added plotting lib and polling for data

--- public/javascripts/sound-project.js
+++ public/javascripts/sound-project.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
 	// Populate the sound data
 	populateSoundDataTable();
 
+	listenForSoundData();
+
 });
 
 // Populate the list of sound data
@@ -13,8 +15,7 @@ function populateSoundDataTable() {
 
 	$.getJSON('/sound-data/sound-data', function(data){
 
-		soundData = data;
-
+		var soundData = data;
 		if(soundData !== null){
 			$.each(soundData, function(){
 				soundDataTableContent += '<tr id="' + this._id + '">';
@@ -28,4 +29,59 @@ function populateSoundDataTable() {
 		$('#soundDataTable table tbody').html(soundDataTableContent);
 
 	});
+
+
+}
+
+function listenForSoundData() {
+
+	var graph;
+
+	$.getJSON('/sound-data/sound-data', function(data){
+
+		var soundData = data;
+		var graphData = '';
+
+		if(soundData !== null){
+			$.each(soundData, function(){
+				graphData += this.timestamp + ',' + this.frequency + '\n';
+			});
+		}
+
+
+		//graph = new Dygraph(document.getElementById("graphdiv"), graphData);
+		graph = new Dygraph(document.getElementById("graphdiv"), graphData,
+			{
+				drawPoints: true,
+				showRoller: true,
+				valueRange: [0,100],
+				labels: ['Timestamp', 'Frequency']
+			}
+		);
+
+	});
+
+
+	window.intervalId = setInterval(function() {
+		// graph.updateOptions( { 'file': graphData } );
+
+		$.getJSON('/sound-data/sound-data', function(data){
+
+			var soundData = data;
+			var graphData = '';
+
+			if(soundData !== null){
+				$.each(soundData, function(){
+					graphData += this.timestamp + ',' + this.frequency + '\n';
+				});
+			}
+
+			graph.updateOptions( { 'file': graphData } );
+
+		});
+
+		populateSoundDataTable();
+
+	}, 2000);
+
 }
