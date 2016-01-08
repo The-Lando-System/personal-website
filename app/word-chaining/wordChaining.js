2fcea24ed7b8c2643e8b20f4b2f064ead7fead19
Matt <matt.voget@gmail.com>
2016-01-07 20:19:03 -0700

    updates to word chainer page

--- app/word-chaining/wordChaining.js
+++ app/word-chaining/wordChaining.js
@@ -1,8 +1,7 @@
 angular.module('myApp.wordChainingController', []).
 controller('WordChainingCtrl', ['$scope', '$http', 'Upload', function ($scope, $http, Upload) {
-	//var wordServiceDomain = 'http://word-service.mattvoget.com';
-	var wordServiceDomain = 'http://localhost:3001';
-	var wordChainerDomain = 'http://localhost:3002';
+	var wordServiceDomain = 'http://word-service.mattvoget.com';
+	var wordChainerDomain = 'http://word-chainer.mattvoget.com';
 	$scope.hello = "Welcome to the word chainer!";
 	$scope.randomWord = '';
 	$scope.getRandomWord = function(){
@@ -16,9 +15,11 @@ controller('WordChainingCtrl', ['$scope', '$http', 'Upload', function ($scope, $
 	$scope.dictionaryOption = 1;
 	$scope.longestChain = {chain:[],time:0};
 	$scope.loading = false;
+	$scope.errorMessage = false;
 
 	$scope.chainWords = function(){
 		$scope.longestChain = {chain:[],time:0};
+		$scope.errorMessage = false;
 		$scope.loading = true;
 		$http.get(wordServiceDomain + '/dictionary/' + $scope.dictionaryOption)
 		.success(function(data){
@@ -27,9 +28,41 @@ controller('WordChainingCtrl', ['$scope', '$http', 'Upload', function ($scope, $
 				console.log(data);
 				$scope.longestChain = data;
 				$scope.loading = false;
+			})
+			.error(function(data){
+				$scope.loading = false;
+				$scope.errorMessage = "Failed to execute the word chainer!";
 			});
+		})
+		.error(function(data){
+			$scope.loading = false;
+			$scope.errorMessage = "Failed to load the dictionary!";
 		});
 		
+	};
+
+	$scope.anagramError = false;
+	$scope.anagrams = [];
+	$scope.anagramInput = '';
+	$scope.anagramLoading = false;
+
+	$scope.findAnagrams = function(){
+		$scope.anagramError = false;
+		$scope.anagramLoading = true;
+		if ($scope.anagramInput.length<8) {
+			$http.get(wordServiceDomain + '/anagrams/' + $scope.anagramInput)
+			.success(function(data){
+				$scope.anagrams = data.anagrams;
+				$scope.anagramLoading = false;
+			})
+			.error(function(data){
+				$scope.anagramError = "Failed to compute anagrams!";
+				$scope.anagramLoading = false;
+			});
+		} else {
+			alert('Use a word less than 8 characters or it will take too long....');
+		}
+		$scope.anagramLoading = false;
 	};
 
 }]);
